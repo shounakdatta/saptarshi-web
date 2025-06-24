ea15629160e884d1ee70df0f3c4fa270cde92f11
Shounak Datta <shounak14datta@gmail.com>
2025-06-24 02:11:30 -0700

    add articles with view counter

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,9 +1,9 @@
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work 👷‍♂️</h1>
       <p className="prose prose-neutral dark:prose-invert">
-        This is where your work experience goes.
+        Coming soon...
       </p>
     </section>
   );
